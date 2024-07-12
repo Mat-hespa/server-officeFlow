d92caa19650e254c4927bfcab861f79a412b575a
Matheus-Hespagnola <matheushespa@outlook.com>
2024-07-12 16:38:39 -0300

    debug controller documento

--- src/documents/documentoService.js
+++ src/documents/documentoService.js
@@ -12,25 +12,31 @@ const createDocumentoDBService = (documentoDetails, documentoFile) => {
       dataCadastro: new Date()
     });
 
-    const nomeArquivo = `${Date.now()}-${documentoFile.name}`;
-    const caminhoArquivo = `uploads/${nomeArquivo}`;
+    // Verifica se documentoFile está definido e tem os atributos esperados
+    if (documentoFile && documentoFile.name && documentoFile.data) {
+      const nomeArquivo = `${Date.now()}-${documentoFile.name}`;
+      const caminhoArquivo = `uploads/${nomeArquivo}`;
 
-    fs.writeFile(caminhoArquivo, documentoFile.data, err => {
-      if (err) {
-        console.error('Erro ao salvar arquivo:', err);
-        reject(false);
-        return;
-      }
-
-      novoDocumento.save()
-        .then(result => {
-          resolve(true);
-        })
-        .catch(error => {
-          console.error('Erro ao cadastrar documento:', error);
+      fs.writeFile(caminhoArquivo, documentoFile.data, err => {
+        if (err) {
+          console.error('Erro ao salvar arquivo:', err);
           reject(false);
-        });
-    });
+          return;
+        }
+
+        novoDocumento.save()
+          .then(result => {
+            resolve(true);
+          })
+          .catch(error => {
+            console.error('Erro ao cadastrar documento:', error);
+            reject(false);
+          });
+      });
+    } else {
+      // Se documentoFile não estiver definido corretamente
+      reject(new Error('Arquivo inválido ou não enviado.'));
+    }
   });
 }
 
