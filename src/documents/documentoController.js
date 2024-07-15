754c58e8e737ca8f52cb73e5375ee9e22c92674a
Matheus-Hespagnola <matheushespa@outlook.com>
2024-07-15 13:52:42 -0300

    ajustes

--- src/documents/documentoController.js
+++ src/documents/documentoController.js
@@ -7,8 +7,8 @@ const Documento = require('./documentoModel'); // Modelo do documento
 
 // Configuração do AWS S3
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION
 });
 
@@ -16,7 +16,7 @@ const s3 = new AWS.S3({
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.AWS_S3_BUCKET_NAME,
+    bucket: process.env.AWS_BUCKET_NAME,
     acl: 'public-read', // Permissão de leitura pública
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
