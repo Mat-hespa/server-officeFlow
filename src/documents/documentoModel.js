e5d7ef6f682c51432e4b505d7db4086549a2bc32
Matheus-Hespagnola <matheushespa@outlook.com>
2024-07-24 09:00:33 -0300

    ajuste

--- src/documents/documentoModel.js
+++ src/documents/documentoModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const documentSchema = new mongoose.Schema({
   registrant: String,
+  recipient: String,
   description: String,
   fileUrl: String,
   createdAt: { type: Date, default: Date.now },
   read: { type: Boolean, default: false },
-  // Novo campo para controlar o fluxo
   status: { type: String, default: 'inicial' },
   history: [
     {
